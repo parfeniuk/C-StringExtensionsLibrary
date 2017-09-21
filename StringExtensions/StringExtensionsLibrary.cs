3b0f1ae2704f4606625d5fb7e572c3a651600440
Admin <o.parfeniuk@gmail.com>
2017-09-21 09:36:45 +0300

    Initial commit

---
+++ StringExtensions/StringExtensionsLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class StringExtensionsLibrary
+    {
+        public static int CountWords(this string str)
+        {
+            int count=0;
+            return count;
+        }
+        public static int CountNonSpecialSymbols(this string str)
+        {
+            int count = 0;
+            return count;
+        }
+    }
+}
